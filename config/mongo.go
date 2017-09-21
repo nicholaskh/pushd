2668f8115c0b5525084578a32f3fb6154a315f6b
unisky <alien_fa@foxmail.com>
2017-09-21 23:41:07 +0800

    add username password

--- config/mongo.go
+++ config/mongo.go
@@ -7,6 +7,9 @@ import (
 
 type ConfigMongo struct {
 	Addr             string
+	Database 	 string
+	UserName	 string
+	Password	 string
 	ConnTimeout      time.Duration
 	OperationTimeout time.Duration
 	SyncTimeout      time.Duration
@@ -14,6 +17,12 @@ type ConfigMongo struct {
 
 func (this *ConfigMongo) LoadConfig(cf *conf.Conf) {
 	this.Addr = cf.String("addr", ":27017")
+	this.Database = cf.String("database", "")
+	this.UserName = cf.String("username", "")
+	this.Password = cf.String("password", "")
+	if this.Database == "" || this.UserName == "" || this.Password == "" {
+		panic("database、username or password in pushd.conf is empty")
+	}
 	this.ConnTimeout = cf.Duration("conn_timeout", time.Second*5)
 	this.OperationTimeout = cf.Duration("operation_timeout", time.Second*5)
 	this.SyncTimeout = cf.Duration("sync_timeout", time.Second*5)
